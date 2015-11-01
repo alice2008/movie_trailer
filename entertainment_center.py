3b3e1d9ae78805a5f547d024acc648a81eb6b0d9
Alice <wuyiapply@gmail.com>
2015-10-31 22:44:20 -0700

    add README.txt

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,25 @@
+from fresh_tomatoes import open_movies_page
+from media import Movie
+def main():
+	forrest_gump = Movie('Forrest Gump', 'Depicts a life of a man',
+		'https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg',
+		'https://www.youtube.com/watch?v=uPIEn0M8su0')
 
-import media
-avatar = media.Movie("avatar", "a man survive on an alien planet", "not url")
-print avatar.story_line
-print avatar.__module__, media.Movie.__name__
-print dir(avatar)
-print avatar.__doc__, media.Movie.__doc__
+	avatar = Movie('Avatar', 'A man tries to save the planet Pandora',
+		'https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg',
+		'https://www.youtube.com/watch?v=5PSNL1qE6VY')
+
+	shawshank = Movie('The Shawshank Redemption', 'A innocent banker tries to escape from prison',
+		'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg',
+		'www.youtube.com/watch?v=6hB3S9bIaco')
+
+	three_idiots = Movie('3 Idiots', 'Two friends are looking for a long-lost friend',
+		'https://upload.wikimedia.org/wikipedia/en/d/df/3_idiots_poster.jpg',
+		'www.youtube.com/watch?v=xvszmNXdM4w')
+
+	movie_list = [forrest_gump, avatar, shawshank, three_idiots]
+	open_movies_page(movie_list)
+
+
+if __name__ == '__main__':
+	main()
--- media.py
+++ media.py
@@ -1,11 +1,13 @@
 
+import webbrowser
 class Movie():
-	"""Movie's doc string."""
+	"""Movie baisc class."""
 
-	def __init__(self, title, story_line, poster_image_url):
+	def __init__(self, title, story_line, poster_image_url, trailer_url):
 		self.title = title
 		self.story_line = story_line
 		self.poster_image_url = poster_image_url
+		self.trailer_youtube_url = trailer_url
 
-	def __showTrailer(self):
-		print self.poster_image_url
+	def showTrailer(self):
+		webbrowser.open(self.trailer_url)
